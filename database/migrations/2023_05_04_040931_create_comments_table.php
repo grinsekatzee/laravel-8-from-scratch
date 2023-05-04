2f9076974989c481e7388940595376b970c5ae78
Greta <greta.ladda@urs-os.de>
2023-05-04 14:45:15 +0200

    04.05.2023

--- database/migrations/2023_05_04_040931_create_comments_table.php
+++ database/migrations/2023_05_04_040931_create_comments_table.php
@@ -13,7 +13,10 @@ return new class extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('post_id');
+            $table->unsignedBigInteger('user_id');
             $table->timestamps();
+            $table->text('body');
         });
     }
 
